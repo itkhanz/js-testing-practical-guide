9eb9db0145a3cbbaecfe1f9e995920208b866269
itkhan <itkhanz@gmail.com>
2022-06-01 21:26:34 +0200

    side effects

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ This repository contains code snapshots and notes taken from JavaScript Unit Tes
 
 ---
 
-## 07 - Mocking & Spie: Dealing with Side Effects
+## 07 - Mocking & Spies: Dealing with Side Effects
+
+### The problem with Side Effects and Tests
+
+While running the testc cases, side effects can occur if you are interacting with the hard drives, databases or other backend APIS that perform some delte, write or other operations that could potentially be harmful.
--- code/app.js
+++ code/app.js
@@ -1,17 +1,5 @@
-import { extractEnteredNumberValues } from "./src/parser.js";
-import { calculateResult } from "./src/math.js";
-import { generateResultText, outputResult } from "./src/output.js";
+import { generateReportData, storeData } from "./mocks-spies/data.js";
+import log from "./mocks-spies/util/logger.js";
 
-const form = document.querySelector("form");
-
-function formSubmitHandler(event) {
-    event.preventDefault();
-    const numberValues = extractEnteredNumberValues(form);
-
-    const result = calculateResult(numberValues);
-    const resultText = generateResultText(result);
-
-    outputResult(resultText);
-}
-
-form.addEventListener("submit", formSubmitHandler);
+const data = generateReportData(log);
+storeData(data);
