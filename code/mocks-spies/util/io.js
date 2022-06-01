135d51446832219ee490be69f72d933d436e0602
itkhan <itkhanz@gmail.com>
2022-06-01 21:57:28 +0200

    custom mock logic

--- code/mocks-spies/util/io.js
+++ code/mocks-spies/util/io.js
@@ -1,8 +1,5 @@
 import path from "path";
 import { promises as fs } from "fs";
-import { vi } from "vitest";
-
-vi.mock("fs");
 
 export default function writeData(data, filename) {
     const storagePath = path.join(process.cwd(), "data", filename);
