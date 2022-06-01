b0a63a3dedfbbe690619f8cbe9bb1ce94aefc542
itkhan <itkhanz@gmail.com>
2022-06-02 00:37:47 +0200

    refreshing test practice

--- README.md
+++ README.md
@@ -17,3 +17,15 @@ While running the testc cases, side effects can occur if you are interacting wit
 ### Mocks
 
 A replacement for an API that may provide some test-specific behavior instead
+
+#### Readings:
+
+[Simple definition of stub, spy, fake and mock in unit testing](https://stackoverflow.com/questions/52131231/simple-definition-of-stub-spy-fake-and-mock-in-unit-testing)
+
+[What's the difference between a mock & stub?](https://stackoverflow.com/questions/3459287/whats-the-difference-between-a-mock-stub)
+
+## 08 - More on Mocking
+
+-   Mocking Global Objects & Functions
+-   Mocking Frontend Features
+-   Examples
--- code/app.js
+++ code/app.js
@@ -1,5 +1,25 @@
-import { generateReportData, storeData } from "./mocks-spies/data.js";
-import log from "./mocks-spies/util/logger.js";
+//Mocks and Spies
+// import { generateReportData, storeData } from "./mocks-spies/data.js";
+// import log from "./mocks-spies/util/logger.js";
 
-const data = generateReportData(log);
-storeData(data);
+// const data = generateReportData(log);
+// storeData(data);
+
+// More Mocking
+import { extractPostData, savePost } from "./posts/posts.js";
+
+const formElement = document.querySelector("form");
+
+export async function submitFormHandler(event) {
+    event.preventDefault();
+
+    const formData = new FormData(formElement);
+    try {
+        const postData = extractPostData(formData);
+        await savePost(postData);
+    } catch (error) {
+        showError(error.message);
+    }
+}
+
+formElement.addEventListener("submit", submitFormHandler);
