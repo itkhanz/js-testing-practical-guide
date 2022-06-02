e6b444666971de65e697d983454f4432494a0eb7
itkhan <itkhanz@gmail.com>
2022-06-02 13:01:13 +0200

    DOM tests

--- code/more-mocking/util/dom.test.js
+++ code/more-mocking/util/dom.test.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-import { it, vi } from "vitest";
+import { beforeEach, expect, it, vi } from "vitest";
 import { Window } from "happy-dom";
 
 import { showError } from "./dom";
@@ -11,10 +11,36 @@ const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
 
 const window = new Window();
 const document = window.document;
-window.location.href = "http://127.0.0.1:8080";
-document.write(htmlDocumentContent);
 vi.stubGlobal("document", document);
 
-it("first test", () => {
-    showError("test");
+beforeEach(() => {
+    document.body.innerHTML = "";
+    document.write(htmlDocumentContent);
+});
+
+it('should add an error paragraph to the id="errors" element', () => {
+    showError("Test");
+
+    const errorsEl = document.getElementById("errors");
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph).not.toBeNull();
+});
+
+it("should not contain an error paragraph initially", () => {
+    const errorsEl = document.getElementById("errors");
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph).toBeNull();
+});
+
+it("should output the provided message in the error paragraph", () => {
+    const testErrorMessage = "Test";
+
+    showError(testErrorMessage);
+
+    const errorsEl = document.getElementById("errors");
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph.textContent).toBe(testErrorMessage);
 });
